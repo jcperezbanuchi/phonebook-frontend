3229ca914ce01425c4da702201c9a8baeacf4ca6
ImaniW <imani@imaniws-mbp.attlocal.net>
2021-06-14 19:53:43 -0400

    pushing react to imani branch

---
+++ phonebook-frontend/src/App.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import NewForm from './components/NewForm';
+import Contact from './components/Contact';
+
+const baseURL = 'http://localhost:3003';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: [],
+      showForm: false,
+      contactUpdate: {}
+    }
+    this.handleAddContact = this.handleAddContact.bind(this)
+    this.updateContact = this.updateContact.bind(this)
+
+    this.toggleForm = this.toggleForm.bind(this)
+    this.deleteContact = this.deleteContact.bind(this)
+  }
+  componentDidMount() {
+    this.getContacts()
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.currentTarget.id]: event.currentTarget.value
+    })
+  }
+
+  getContacts() {
+    fetch(baseURL + '/contacts')
+      .then(data => { return data.json() }, err => console.log(err))
+      .then(parsedData => this.setState({ contacts: parsedData }), err => console.log(err))
+  }
+
+
+  toggleForm(contact) {
+    this.setState({
+      showForm: true,
+      contactUpdate: contact
+    })
+  }
+
+  handleAddContact(contact) {
+    const copyContacts = [...this.state.contacts]
+    copyContacts.unshift(contact)
+    this.setState({
+      contacts: copyContacts,
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      address: '',
+      socialLinks: ''
+    })
+  }
+
+
+  updateContact(contact) {
+    contact.preventDefault()
+    fetch(`${baseURL}/contacts/${contact._id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        phoneNumber: contact.phoneNumber,
+        address: contact.address,
+        socialLinks: contact.socialLinks
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .then(resJson => {
+        const copyContacts = [...this.state.contacts]
+        const findIndex = this.state.contacts.findIndex(contact => contact._id === resJson._id)
+        copyContacts[findIndex].firstName = resJson.firstName
+        copyContacts[findIndex].lastName = resJson.lastName
+        copyContacts[findIndex].phoneNumber = resJson.phoneNumber
+        copyContacts[findIndex].address = resJson.address
+        copyContacts[findIndex].socialLinks = resJson.socialLinks
+        this.setState({ contacts: copyContacts })
+      })
+    this.setState({
+      showForm: false
+    })
+
+  }
+
+  deleteContact(id) {
+    fetch(`${baseURL}/contacts/${id}`, {
+      method: 'DELETE'
+    })
+      .then(res => {
+        if (res.status === 200) {
+          const findIndex = this.state.contacts.findIndex(contact => contact._id === id)
+          const copyContacts = [...this.state.contacts]
+          copyContacts.splice(findIndex, 1)
+          this.setState({
+            contacts: copyContacts
+          })
+        }
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        
+        {
+          this.state.showForm
+            ?
+            <Contact updateContact={this.updateContact} contactUpdate={this.state.contactUpdate} />
+            :
+            <NewForm className="new-form" handleAddContact={this.handleAddContact} />
+        }
+        <div className="form">
+          <table>
+            <tbody>
+              {this.state.contacts.map(contact => {
+                return (
+                  <tr key={contact._id}>
+                    <td><a href={contact.lastName}>{contact.firstName}</a></td>
+                    <td><button className="btn btn-info" onClick={() => this.toggleForm(contact)}>Edit</button></td>
+                    <td ><button className="btn btn-danger" onClick={() => this.deleteContact(contact._id)}>Delete</button></td>
+                  </tr>
+                )
+              })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div >
+    )
+  }
+}
+
+export default App;
---
+++ phonebook-frontend/src/components/Contact.jsx
@@ -0,0 +1,100 @@
+import { Component } from 'react';
+
+const baseURL = 'http://localhost:3003';
+
+
+class Contact extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            _id: '',
+            firstName: '',
+            lastName: '',
+            phoneNumber: '',
+            address: '',
+            socialLinks: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
+    }
+    componentDidMount() {
+        this.setUpdateContact();
+    }
+    setUpdateContact() {
+        this.setState({
+            _id: this.props.contactUpdate._id,
+            firstName: this.props.contactUpdate.firstName,
+            lastName: this.props.contactUpdate.lastName,
+            phoneNumber: this.props.contactUpdate.phoneNumber,
+            address: this.props.contactUpdate.address,
+            socialLinks: this.props.contactUpdate.socialLinks
+        })
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.currentTarget.id]: event.currentTarget.value
+        })
+    }
+    handleSubmit(event) {
+        fetch(baseURL + '/contacts' + this.props.contactUpdate._id, {
+            method: 'PUT',
+            body: JSON.stringify({
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                phoneNumber: this.state.phoneNumber,
+                address: this.state.address,
+                socialLinks: this.state.socialLinks
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(resJson => {
+                this.props.updateContact(resJson)
+                this.setState({
+                    firstName: '',
+                    lastName: '',
+                    phoneNumber: '',
+                    address: '',
+                    socialLinks: ''
+                })
+            })
+            .catch(error => console.log({ 'Error': error }))
+    };
+
+
+
+    render() {
+        return (
+            <div>
+                {/* <h3>
+                    Edit Contact
+                </h3> */}
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="firstName"></label>
+                <input type="text" name="firstName" id="firstName" value={this.state.firstName} onChange={this.handleChange} placeholder={this.state.firstName} />
+                
+                <label htmlFor="lastName"></label>
+                <input type="text" name="lastName" id="lastName" value={this.state.lastName} onChange={this.handleChange} placeholder={this.state.lastName} />
+                
+                <label htmlFor="phoneNumber"></label>
+                <input type="text" name="phoneNumber" id="phoneNumber" value={this.state.phoneNumber} onChange={this.handleChange} placeholder={this.state.phoneNumber} />
+                
+                <label htmlFor="address"></label>
+                <input type="text" name="address" id="address" value={this.state.address} onChange={this.handleChange} placeholder={this.state.address} />
+
+                <label htmlFor="socialLinks"></label>
+                <input type="text" name="socialLinks" id="socialLinks" value={this.state.socialLinks} onChange={this.handleChange} placeholder={this.state.socialLinks} />
+                
+                <input className="btn btn-dark" type="submit" value="Save Changes" />
+
+            </form>
+            </div>
+        )
+    }
+};
+
+export default Contact;
