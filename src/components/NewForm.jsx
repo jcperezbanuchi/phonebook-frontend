2e3737344e48f2fdd57c450ebcea573a86dff9cc
Juanco <juanco@Juans-Air>
2021-06-15 14:17:31 -0400

    fixing front end

--- src/components/NewForm.jsx
+++ src/components/NewForm.jsx
@@ -29,7 +29,7 @@ class NewForm extends Component {
 
   handleSubmit(event) {
     event.preventDefault()
-    fetch(baseURL + '/contacts', {
+    fetch(`${baseURL}/contacts`, {
       method: 'POST',
       body: JSON.stringify(
         {
